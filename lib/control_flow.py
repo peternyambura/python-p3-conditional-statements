53ad10948c1d9f8cfeb1294d29342385d43063a7
= <peterndirangu0005@gmail.com>
2023-09-13 16:12:07 +0300

    Update

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
+    if username.ascii_lowercase() == "admin" and password =="1234":
+        return "Accsess granted"
+    else:
+        return "Accsess Denied, Contact Admin for Help"
     pass
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
-
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif 40 <= temperature <= 65:
+        return "It's a little chilly out there!"
+    elif temperature >=85:
+        return "It's too dang hot out there!"
+    else:
+        return "Its perfect out there"
+    
 def fizzbuzz(num):
     # your code here
     pass
